c6d4f9c159308d37d73115c84e6a7ab2011cb00a
Kaiwen Yang <92202424+kai-wen-yang@users.noreply.github.com>
2023-11-08 22:36:44 +0800

    Update eval.sh

--- LVLM_evaluation/eval.sh
+++ LVLM_evaluation/eval.sh
@@ -1,3 +1,8 @@
+export TORCH_HUB=/fs/nexus-scratch/kwyang3/models
+export TRANSFORMERS_CACHE=/fs/nexus-scratch/kwyang3/models
+export HF_DATASETS_CACHE=/fs/nexus-scratch/kwyang3/data
+export WANDB_CACHE_DIR=/fs/nexus-scratch/kwyang3/.cache
+
 python eval.py \
 --model_name InstructBLIP \
 --device 0 \
